ddce50280bbd1cc5b735c8878679a5be2760196a
Radlog <sergej.frank1990@gmail.com>
2024-10-02 19:01:04 +0200

    added util methods for quicker conversion of java into dart files

--- lohnsteuer/lib/lohnsteuerUtil.dart
+++ lohnsteuer/lib/lohnsteuerUtil.dart
@@ -12,3 +12,53 @@ Decimal roundDown(Decimal value, int scale) {
   final Decimal scaledValue = (value * multiplier).floor();
   return (scaledValue / multiplier).toDecimal();
 }
+
+enum Round{
+  DOWN,
+  UP
+}
+
+extension Divide on Decimal {
+  Decimal divide(Decimal other, int scale, Round round) {
+    if (round == Round.UP) {
+      return roundUp(this * other, scale);
+    }
+    else
+    {
+      return roundDown(this * other, scale);
+    }
+  }
+
+  Rational dividePlain(Decimal other) {
+    return this / other;
+  }
+}
+
+extension Multiply on Decimal {
+  Decimal multiply(Decimal other, int scale, Round round) {
+    if (round == Round.UP) {
+      return roundUp(this * other, scale);
+    }
+    else
+    {
+      return roundDown(this * other, scale);
+    }
+  }
+
+  Decimal multiplyPlain(Decimal other) {
+      return this * other;
+  }
+}
+
+extension Add on Decimal {
+  Decimal add(Decimal other) {
+      return this + other;
+    }
+}
+
+extension RoundUp on Decimal {
+  Decimal round_Up()
+  {
+    return roundUp(this, 2);
+  }
+}

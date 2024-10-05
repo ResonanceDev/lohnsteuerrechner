cc8842ca8f039d8f78656e850f75bd4686caf68a
RobinDev <149971508+ResonanceDev@users.noreply.github.com>
2024-10-05 11:02:23 +0200

    Modified params

--- lohnsteuer/test/lohnsteuer_test.dart
+++ lohnsteuer/test/lohnsteuer_test.dart
@@ -6,16 +6,24 @@ import 'package:test/test.dart';
 void main() {
   group('A group of tests', () {
     final awesome = Awesome();
-    final dt = DateTime(2024);
+    final dt = DateTime(2024, 1,1 ,1);
 
     final lohnsteuer = Lohnsteuer.getInstance(dt);
 
     lohnsteuer.setRe4(BigDecimal(350000)); // -> WICHTIGSTE ANGABE -> BURTTO-LOHN
     lohnsteuer.setVjahr(2024);
     lohnsteuer.setR(0);
-    lohnsteuer.setJre4(BigDecimal(0)); // -> Herausfinden was das ist
+    //lohnsteuer.setJre4(BigDecimal(0)); // -> Herausfinden was das ist
     lohnsteuer.setLzz(2);
     lohnsteuer.setStkl(1);
+    lohnsteuer.setAlter1(0);
+    lohnsteuer.setKvz(BigDecimal(1.7));
+    lohnsteuer.setPkv(0);
+    lohnsteuer.setKrv(0);
+    lohnsteuer.setAf(0);
+
+
+
 
     lohnsteuer.main();
     print("\nLohnabzuege bei 3500 EUR Brutto: \n\n");
